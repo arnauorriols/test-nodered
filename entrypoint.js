265d27fd0f517e0a08b8971a4ca29154b70e14a3
Arnau Orriols <arnauorriolsmiro@gmail.com>
2025-04-16 11:18:59 +0200

    Set userdir

--- entrypoint.js
+++ entrypoint.js
@@ -1,2 +1,2 @@
-const proc = await new Deno.Command("deno", {args: ["run", "-A", "--unstable-detect-cjs", "npm:node-red"], stdout: "inherit", "stderr": "inherit" }).output();
+const proc = await new Deno.Command("deno", {args: ["run", "-A", "--unstable-detect-cjs", "npm:node-red", "--userDir", "./user"], stdout: "inherit", "stderr": "inherit" }).output();
 console.log(proc);
